f8e214ea7154f74b0df6922002913abf67780fd2
fabriziorosichini <127482458+fabriziorosichini@users.noreply.github.com>
2023-04-22 09:59:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int i=0, n=0, x1=0, x2=0, x3=0;
+    x1=1;
+    x2=1;
+    x3=x1+x2;
+    cin>>n;
+
+    if (n>=2){
+        cout<<x1<<endl<<x2<<endl;
+        for (i=2; i<n; i++){
+            x3=x1+x2;
+            x1=x2;
+            x2=x3;
+            cout<<x3<<endl;
+        }
+    }else{
+        cout<<"errore";
+    }
+
+
+    return 0;
 }
